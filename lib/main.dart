ba843b84dca45f69c05c6d82d7846a86cff586a3
Gabriel Bonilla <gpbonillas@gmail.com>
2021-02-25 14:36:20 -0500

    Adding BottomNavigationBar widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'description_place.dart';
-import 'review_list.dart';
-import 'gradient_back.dart';
-import 'header_appbar.dart';
+import 'platzi_trips.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,14 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
-  String descriptionDummy = "Lorem Ipsum is simply dummy text of the printing "
-      "and typesetting industry. Lorem Ipsum has been the industry's standard "
-      "dummy text ever since the 1500s, when an unknown printer took a galley "
-      "of type and scrambled it to make a type specimen book. \n\nLorem Ipsum "
-      "is simply dummy text of the printing and typesetting industry. Lorem "
-      "Ipsum has been the industry's standard dummy text ever since the 1500s, "
-      "when an unknown printer took a galley of type and scrambled it to make a "
-      "type specimen book.";
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,19 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            ListView(
-              children: <Widget>[
-                DescriptionPlace("Bahamas", 4, descriptionDummy),
-                ReviewList(),
-              ],
-            ),
-            HeaderAppBar()
-          ],
-        ),
-      )
+      home: PlatziTrips()
     );
   }
 }
