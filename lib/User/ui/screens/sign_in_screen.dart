1636a86f30ca1af6f213f17ed5c7f4eeb5baa4cb
Gabriel Bonilla <gpbonillas@gmail.com>
2021-03-10 00:03:43 -0500

    Create login screen with Flutter

---
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/widgets/gradient_back.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return signInGoogleUI();
+  }
+
+  Widget signInGoogleUI() {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          GradientBack("", null),
+          Column(
+            children: [
+              Text(
+                "Welcome \n This is your Travel App",
+                style: TextStyle(
+                  fontSize: 37.0,
+                  fontFamily: "Lato",
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
